4cf3df47097b33da96c7d6cdfcb3c0a9b0ab0198
derry10 <122221930+derry10@users.noreply.github.com>
2024-03-23 19:21:24 +0700

    Update database.go

--- MyGram/database/database.go
+++ MyGram/database/database.go
@@ -1,43 +1,36 @@
-package database
-
 import (
 	"fmt"
 	"log"
-	"mygram/models"
 	"os"
+	"mygram/models"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var (
-	host     = os.Getenv("DB_HOST")
-	port     = os.Getenv("DB_PORT")
-	user     = os.Getenv("DB_USER")
-	password = os.Getenv("DB_PASSWORD")
-	dbname   = os.Getenv("DB_DATABASE")
+	host     = os.Getenv("MYSQL_HOST")
+	port     = os.Getenv("MYSQL_PORT")
+	user     = os.Getenv("MYSQL_USER")
+	password = os.Getenv("MYSQL_PASSWORD")
+	dbname   = os.Getenv("MYSQL_DATABASE")
 
 	db  *gorm.DB
 	err error
 )
 
 func StartDB() {
-	// Database connection configuration for MySQL
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	config := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, password, host, port, dbname)
 
-	// Open a connection to MySQL
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+	db, err = gorm.Open(mysql.Open(config), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect to the database: ", err)
+		log.Fatal("error connection to database:", err)
 	}
 
-	// Auto migrate model tables
-	err = db.AutoMigrate(&models.User{}, &models.Comment{}, &models.Photo{}, &models.SocialMedia{})
-	if err != nil {
-		log.Fatal("Failed to auto migrate tables: ", err)
-	}
+	fmt.Println("Successfully connected to the database")
+
+	db.Debug().AutoMigrate(&models.User{}, &models.Comment{}, &models.Photo{}, &models.SocialMedia{})
 }
 
 func GetDB() *gorm.DB {
